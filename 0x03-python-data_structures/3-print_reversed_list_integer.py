c7890556ccf21fa9c9b32277b93a9c37c3e224ea
Simon Parra <simon.pahe@gmail.com>
2019-12-17 18:57:52 +0000

    changing str.format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 def print_reversed_list_integer(my_list=[]):
-    str = "{}"
     my_list.reverse()
     for i in range(len(my_list)):
-        print(str.format(my_list[i]))
+        print("{:d}".format(my_list[i]))
