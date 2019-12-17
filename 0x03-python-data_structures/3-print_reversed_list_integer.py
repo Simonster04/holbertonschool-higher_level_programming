f329bc2c34e7b4a55b097c16aeea8dc2653a7878
Simon Parra <simon.pahe@gmail.com>
2019-12-17 19:10:20 +0000

    removing .reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    for i in range(len(my_list) + 1):
+        if i == 0:
+            continue
+        print("{:d}".format(my_list[-i]))
