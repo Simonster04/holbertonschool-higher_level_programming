4d00e880f72913be26b0d6e7535e4ae1ef3db175
Simonster04 <simon.pahe@gmail.com>
2019-12-18 00:19:51 +0000

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    lena, lenb = len(tuple_a), len(tuple_b)
-    sum_tuple = ((tuple_a[0] if lena >= 1 else 0) + (tuple_b[0] if lenb >= 1 else 0),
-                (tuple_a[1] if lena >= 2 else 0) + (tuple_b[1] if lenb >= 2 else 0))
+    a = len(tuple_a)
+    b = len(tuple_b)
+    sum_tuple = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+                 (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
     return sum_tuple
