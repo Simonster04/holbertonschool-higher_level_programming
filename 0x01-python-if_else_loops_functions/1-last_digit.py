e75a50f21cc6474ae93cdee0f5c8ff8e1d873196
Simon Parra <simon.pahe@gmail.com>
2019-12-18 20:11:18 +0000

    changing len in line 10

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdig = abs(number) % 10
+lastd = abs(number) % 10
 if number < 0:
-    lastdig *= -1
-if lastdig > 5:
-    print("Last digit of %d is %d and is greater than 5" % (number, lastdig))
-elif lastdig < 6 and lastdig != 0:
-    print("Last digit of %d is %d and is less than 6 and not 0" % (number, lastdig))
+    lastd *= -1
+if lastd > 5:
+    print("Last digit of %d is %d and is greater than 5" % (number, lastd))
+elif lastd < 6 and lastd != 0:
+    print("Last digit of %d is %d and is less than 6 and not 0" % (number, lastd))
 else:
-    print("Last digit of %d is %d and is 0" % (number, lastdig))
+    print("Last digit of %d is %d and is 0" % (number, lastd))
