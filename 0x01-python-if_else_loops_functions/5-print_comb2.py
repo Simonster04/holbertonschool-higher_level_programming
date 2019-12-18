dffd5b07b1933095bcec308355bed8aa370f416f
Simon Parra <simon.pahe@gmail.com>
2019-12-18 23:25:12 +0000

    print numbers with ,(space)

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
+    if i < 10:
+        print("{:d}".format(0), end="")
     print("{:d}".format(i), end="")
     if i < 99:
         print(", ", end="")
