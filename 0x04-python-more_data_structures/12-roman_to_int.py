aefa6765caf82c640bab5b4481e3ef74d2bb15f5
Simonster04 <simon.pahe@gmail.com>
2019-12-19 22:15:38 +0000

    adding spaces between keys and values

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str:
         return 0
-    d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     i = 0
     res = 0
     while i < len(roman_string):
