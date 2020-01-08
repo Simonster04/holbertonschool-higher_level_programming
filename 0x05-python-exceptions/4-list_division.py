d6d3d66a812ad30c7154935bfeed0a51ec080a2b
Simonster04 <simon.pahe@gmail.com>
2020-01-08 23:35:41 +0000

    adding various except cases

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for i in range(list_length):
-            if type(my_list_1[i]) != int or type(my_list_2[i]) != int:
-                print("wrong type")
-                new_list.append(0)
-            else:
-                new_list.append(my_list_1[i] / my_list_2[i])
-        return new_list
-    except ZeroDivisionError:
-        print("division by 0")
-        new_list.append(0)
-    finally:
-        return new_list
+    for i in range(list_length):
+        try:
+            new = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            new = 0
+        except IndexError:
+            print("out of range")
+            new = 0
+        except (ValueError, TypeError):
+            print("wrong type")
+            new = 0
+        finally:
+            new_list.append(new)
+    return new_list
