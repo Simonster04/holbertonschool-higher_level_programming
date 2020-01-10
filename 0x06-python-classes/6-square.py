4b82e3082bb9843c9b969f7e006d62328129e082
Simonster04 <simon.pahe@gmail.com>
2020-01-10 03:01:51 +0000

    adding conditions to def print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.size == 0:
+        if self.__size == 0:
             print()
         else:
             for z in range(self.__position[1]):
