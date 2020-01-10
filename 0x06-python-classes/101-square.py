a095ac2665b8fe5769a072bc20da5a93735c4bce
Simonster04 <simon.pahe@gmail.com>
2020-01-10 03:25:32 +0000

    advanced task

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        msg = "position must be a tuple of 2 positive integers"
+        if type(value) is tuple:
+            if len(value) != 2:
+                raise TypeError(msg)
+            else:
+                for i in range(len(value)):
+                    if value[i] < 0 or type(value[i]) != int:
+                        raise TypeError(msg)
+            self.__position = value
+        else:
+            raise TypeError(s)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for z in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__size + self.__position[0]):
+                    if j < self.__position[0]:
+                        print(" ", end="")
+                    else:
+                        print("#", end="")
+                print()
+
+    def __str__(self):
+        if self.__size == 0:
+            return ("")
+        else:
+            string = ""
+            for z in range(self.__position[1]):
+                string += "\n"
+            for i in range(self.__size):
+                for j in range(self.__size + self.__position[0]):
+                    if j < self.__position[0]:
+                        string += " "
+                    else:
+                        string += "#"
+                string += "\n"
+            string = string[:-1]
+            return string
