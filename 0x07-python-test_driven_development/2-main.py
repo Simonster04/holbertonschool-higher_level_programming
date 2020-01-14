38ca5afe9388457df0b6fbd5d4761a4179d85f10
Simonster04 <simon.pahe@gmail.com>
2020-01-14 03:50:12 +0000

    adding more conditions to task 2

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
+matrix = [[1,2], [1, 2, 4]]
+
 print(matrix_divided(matrix, 3))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3
+"""
+ This is the 2-matrix_divided module
+ containing the matrix_divided function
+ returns all elements of a matrix divided
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+     matrix_divided function
+    """
+    mat_err = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(matrix, list):
+        raise TypeError(mat_err)
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if matrix == [[]] or matrix == []:
+        raise TypeError(mat_err)
+    aux = matrix[0]
+    for row in matrix:
+        if not isinstance(row, list) or row == []:
+            raise TypeError(mat_err)
+        if len(row) != len(aux):
+            raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(row, list):
+            raise TypeError(mat_err)
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError(mat_err)
     return [[round((x / div), 2) for x in row] for row in matrix]
