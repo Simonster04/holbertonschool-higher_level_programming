fd51e9da996db2ef2c4add7aabede6f087175d3a
Simonster04 <simon.pahe@gmail.com>
2020-01-14 04:53:48 +0000

    2nd advanced

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+ This module contains the function lazy_matrix_mul
+
+"""
+import numpy
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+     multiplies 2 matrices with Numpy
+    """
+    return numpy.matmul(m_a, m_b)
