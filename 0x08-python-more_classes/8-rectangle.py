d1c81a823f30c2b8e88f109cd78abca0e0cec736
Simonster04 <simon.pahe@gmail.com>
2020-01-15 04:52:03 +0000

    docstr

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -67,3 +67,15 @@ class Rectangle:
         """ Print a message when you delete the rectangle"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ compares two rectangles """
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
