dc205e4aab9de4ed9901cd089f1db20500849964
Simonster04 <simon.pahe@gmail.com>
2020-01-15 05:14:32 +0000

    fixing classmethod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -84,4 +84,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ returns a new Rectangle instance with wid == hei == siz """
-        return cls(size, size)
+        return Rectangle(size, size)
