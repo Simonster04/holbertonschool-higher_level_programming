c8867e7a4bf00da269c7858facc3ae039af1d017
Simonster04 <simon.pahe@gmail.com>
2020-01-21 22:14:48 +0000

    PEP8

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,8 +6,7 @@
 
 def append_write(filename="", text=""):
     """ appends a string at the end of a text file (UTF8)
-     and returns the number of characters added
-    """
+     and returns the number of characters added """
     with open(filename, mode="a", encoding="utf-8") as myFile:
-        myFile.write(text)   
-        return len(text)
+        myFile.write(text)
+    return len(text)
