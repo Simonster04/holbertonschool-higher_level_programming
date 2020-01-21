d0d8e5e12d2870dcc985f6ff715ca63701f7585a
Simonster04 <simon.pahe@gmail.com>
2020-01-21 22:08:09 +0000

    adding <= condition

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,7 +7,7 @@
 def read_lines(filename="", nb_lines=0):
     """ reads n lines of textfile-UTF8 and prints to stdout """
     with open(filename, encoding="utf-8") as myFile:
-        if nb_lines == 0:
+        if nb_lines <= 0:
             print(myFile.read(), end="")
         for i in range(nb_lines):
             print(myFile.readline(), end="")
