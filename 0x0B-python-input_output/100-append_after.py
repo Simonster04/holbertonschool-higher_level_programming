de7f7d9b37e1cf6417c9c9c15c84b66ac4f3f046
Simonster04 <simon.pahe@gmail.com>
2020-01-22 04:09:06 +0000

    adding task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ Contains append_after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file, after each
+     line containing a specific string """
+    with open(filename, encoding="utf-8") as myFile:
+        linelist = []
+        while True:
+            line = myFile.readline()
+            linelist.append(line)
+            if search_string in line:
+                linelist.append(new_string)
+            if not line:
+                break
+    with open(filename, mode = "w", encoding="utf-8") as myFile:
+        myFile.writelines(linelist)
