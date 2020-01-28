305bfb116c579724640bae279a1f904124cb0020
Simonster04 <simon.pahe@gmail.com>
2020-01-27 21:12:25 -0500

    uploading project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,10 @@ class Base:
             else:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 myFile.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string """
+        if json_string is None:
+            return "[]"
+        return json.loads(json_string)
