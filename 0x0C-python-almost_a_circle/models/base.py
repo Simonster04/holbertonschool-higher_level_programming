490b734de66393e8cbb7a1928eee3325567471dc
Simonster04 <simon.pahe@gmail.com>
2020-01-27 19:48:26 -0500

    upgrading project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,5 +19,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None or list_dictionaries == []:
-            list_dictionaries = []
+            return "[]"
         return json.dumps(list_dictionaries)
