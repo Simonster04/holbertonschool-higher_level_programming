b6d2d07d766b31fe3bc169bbb45b66acaa3fc9f7
Simonster04 <simon.pahe@gmail.com>
2020-03-02 17:17:17 -0500

    fixing average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Compute the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server
 -- The database name will be passed as an argument of the mysql command
-SELECT AVG(score) AS 'Average' FROM second_table;
+SELECT AVG(score) AS 'average' FROM second_table;
