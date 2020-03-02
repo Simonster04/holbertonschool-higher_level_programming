f50563fcbef29ac19a2c3c41776e2e8cdeb6a6f1
Simonster04 <simon.pahe@gmail.com>
2020-03-02 17:27:12 -0500

    deleting WHERE condition, adding DESC

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- The database name will be passed as an argument to the mysql command
-SELECT score, name FROM second_table WHERE name != "" ORDER by score;
+SELECT score, name FROM second_table ORDER by score DESC;
