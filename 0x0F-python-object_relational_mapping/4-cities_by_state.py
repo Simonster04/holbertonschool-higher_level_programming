fb63007097ac7e3cd1000e5e7188ebbe01651f2c
Simonster04 <simon.pahe@gmail.com>
2020-03-28 15:38:46 -0500

    fixing SQL query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
