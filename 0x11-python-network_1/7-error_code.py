9fcbf9936d3d27f253e9dad38f2d7e7605c729b3
Simonster04 <simon.pahe@gmail.com>
2020-04-15 17:42:42 -0500

    fixing condition

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if r.status_code < 400:
+    if r.status_code == 200:
         print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
