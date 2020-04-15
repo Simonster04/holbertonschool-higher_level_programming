3e31f0ac40d014d8d8e7430eac14d3eb0a37f9e3
Simonster04 <simon.pahe@gmail.com>
2020-04-15 16:01:34 -0500

    fixing request

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(argv[1], data) as response:
+        with urllib.request.urlopen(argv[1]) as response:
             html = response.read().decode('utf-8')
             print(html)
     except urllib.error.HTTPError as error:
