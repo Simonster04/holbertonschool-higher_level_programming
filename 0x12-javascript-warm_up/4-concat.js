68ead12cbd6cccdb357e35f1ea3491f506a48b68
Simonster04 <simon.pahe@gmail.com>
2020-05-05 15:01:18 -0500

    removing unnecesary conditions

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2]) {
-  console.log(process.argv[2] + ' is undefinded');
-} else if (process.argv[3]) {
-  console.log('undefinded is ' + process.argv[2]);
-} else {
-  console.log('undefinded is undefinded');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
