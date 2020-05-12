c2603175c284c4424a2ea8d2b49a93f4b2bd0bdf
Simonster04 <simon.pahe@gmail.com>
2020-05-12 16:46:10 -0500

    adding error condition if error ocurres

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,9 @@
 const request = require('request');
 const fs = require('fs');
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
+  if (error) {
+    console.log(error);
+  } else {
     fs.writeFile(process.argv[3], body, 'utf8');
   }
 });
