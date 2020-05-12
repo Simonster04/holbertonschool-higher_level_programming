6b070aa244b5ef0f51bb989b4c04ec63733bde75
Simonster04 <simon.pahe@gmail.com>
2020-05-12 17:04:55 -0500

    adding error condition to writeFile

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,9 +3,11 @@
 const request = require('request');
 const fs = require('fs');
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf8');
+  if (!error) {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
